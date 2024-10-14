8fcaa5038bf3eb13324d189a61f2abfedf38b170
jesusmarianuin <jesusmarianuin@gmail.com>
2024-10-14 22:50:48 +0200

    progreso web3mba

---
+++ contracts/mba/MusicToken.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MusicToken is ERC721 {
+    struct Song {
+        string title;
+        string artist;
+        uint256 year;
+        bytes32 hash;
+    }
+    mapping(uint256 => Song) public songs;
+    mapping(bytes32 => uint256) public hashesToIds;
+    uint256 totalSupply;
+
+    constructor () ERC721("MusitToken", "MST"){}
+
+    // es plataforma abierta asi que no es ownable
+    function createSong(string memory _title, string memory _artist, uint256  _year, bytes32  _hash) public {
+        totalSupply++;
+        songs[totalSupply] = Song(_title, _artist, _year, _hash);
+        hashesToIds[_hash] = totalSupply;
+    }
+
+}
